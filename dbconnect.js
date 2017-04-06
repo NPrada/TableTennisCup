294f8c063a2c525a3f22f9e023765ecc6d81ace5
Nimajik <niccolo.prada@gmail.com>
2017-04-06 15:17:38 +0100

    Matchpage for normal user fully working
    some small tweaks are still needed

--- dbconnect.js
+++ dbconnect.js
@@ -28,27 +28,22 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get("/matches", function (req,res) {
+app.get("/matches/:matchID", function (req,res) {
 
+    var matchID = req.params.matchID;
 
-    connection.query("SELECT * FROM matches", function (error, rows, fields){
-        // callback aka when the query is done this fires
+    connection.query("SELECT matches.date, matches.postponed, matches.scores, ht.teamName AS homeTeam, at.teamName AS awayTeam " +
+        "FROM matches " +
+        "INNER JOIN teams ht ON ht.id=matches.hteam " +
+        "INNER JOIN teams at ON at.id=matches.ateam " +
+        "WHERE matches.id="+matchID, function (error, rows, fields){
+        // callback aka when the query is matches.iddone this fires
         if (!!error){
             console.log("Error in the query");
         } else {
-            console.log("Successful query");
-            //console.log(rows[0].name);
-            //console.log(rows[0]);
-            //console.log(rows);
-            //res.send(rows);
-            //res.write('you posted:\n');
-            //var x = rows;
-
-            //res.send((rows[0].id).toString());        //this sends a 1
-            res.send(rows[0]);
-            //res.send(rows);
-
-
+            console.log(rows);
+            res.send(rows);  //rows [0] because there should only be 1 row returned anyway (makes the code later cleaner)
+            console.log("Match info Successful query");
         }
     });
 });
@@ -57,15 +52,20 @@ app.get("/singleSets/:matchID", function (req,res) {
 
     var matchID = req.params.matchID;                                                                                   //this fetches the endpoint call
 
-    connection.query("SELECT singleset.id, singleset.matchid, p2.name AS hplayer, p2.handicap AS hhandicap,  p1.name AS aplayer, p1.handicap AS ahandicap, singleset.g1h, singleset.g1a, singleset.g2h, singleset.g2a, singleset.g3h, singleset.g3a, singleset.g4h, singleset.g4a FROM singleset INNER JOIN players p1 ON p1.id=singleset.aplayer INNER JOIN players p2 ON p2.id=singleset.hplayer WHERE singleset.matchid="+matchID, function (error, rows, fields){
+    connection.query("SELECT singleset.id, singleset.matchid, p2.name AS hplayer, p2.handicap AS hhandicap,  " +
+        "p1.name AS aplayer, p1.handicap AS ahandicap, singleset.g1h, singleset.g1a, singleset.g2h, singleset.g2a, " +
+        "singleset.g3h, singleset.g3a, singleset.g4h, singleset.g4a FROM singleset " +
+        "INNER JOIN players p1 ON p1.id=singleset.aplayer " +
+        "INNER JOIN players p2 ON p2.id=singleset.hplayer " +
+        "WHERE singleset.matchid="+matchID, function (error, rows, fields){
 
         // callback aka when the query is done this fires
         if (!!error){
             console.log("Error in the query");
             console.log(error);
         } else {
-            console.log(rows);
-            console.log("Successful query");
+            //console.log(rows);
+            console.log("Single Sets Successful query");
             res.send(rows);
         }
     });
@@ -76,7 +76,13 @@ app.get("/doubleSets/:matchID", function (req,res) {
 
     var matchID = req.params.matchID;                                                                                   //this fetches the endpoint call
 
-    connection.query("SELECT doubleset.id, doubleset.matchid, p1.name AS hplayer1 , p1.handicap AS hhandicap1, p2.name AS hplayer2, p2.handicap AS hhandicap2, p3.name AS aplayer1,p3.handicap AS ahandicap1, p4.name AS aplayer2,p4.handicap AS ahandicap2, doubleset.g1h, doubleset.g1a, doubleset.g2h, doubleset.g2a, doubleset.g3h, doubleset.g3a, doubleset.g4h, doubleset.g4a FROM doubleset INNER JOIN players p1 ON p1.id=doubleset.hP1 INNER JOIN players p2 ON p2.id=doubleset.hP2 INNER JOIN players p3 ON p3.id=doubleset.aP1 INNER JOIN players p4 ON p4.id=doubleset.aP2 WHERE doubleset.matchID="+matchID, function (error, rows, fields){
+    connection.query("SELECT doubleset.id, doubleset.matchid, p1.name AS hplayer1 , p1.handicap AS hhandicap1, " +
+        "p2.name AS hplayer2, p2.handicap AS hhandicap2, p3.name AS aplayer1,p3.handicap AS ahandicap1, " +
+        "p4.name AS aplayer2,p4.handicap AS ahandicap2, doubleset.g1h, doubleset.g1a, doubleset.g2h, " +
+        "doubleset.g2a, doubleset.g3h, doubleset.g3a, doubleset.g4h, doubleset.g4a FROM doubleset " +
+        "INNER JOIN players p1 ON p1.id=doubleset.hP1 INNER JOIN players p2 ON p2.id=doubleset.hP2 " +
+        "INNER JOIN players p3 ON p3.id=doubleset.aP1 INNER JOIN players p4 ON p4.id=doubleset.aP2 " +
+        "WHERE doubleset.matchID="+matchID, function (error, rows, fields){
 
         // callback aka when the query is done this fires
         if (!!error){
@@ -84,7 +90,7 @@ app.get("/doubleSets/:matchID", function (req,res) {
             console.log(error);
         } else {
             console.log(rows);
-            console.log("Successful query");
+            console.log("Double sets Successful query");
             res.send(rows);
         }
     });
