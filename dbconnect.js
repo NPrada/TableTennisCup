290d89c4811e6a227bb7a83d5d45eb70837e1339
Nimajik <niccolo.prada@gmail.com>
2017-04-11 13:43:06 +0100

    updating the singles players works

--- dbconnect.js
+++ dbconnect.js
@@ -249,7 +249,6 @@ app.get("/playersOfTeam/:teamID", function (req,res) {
 
 });
 //update a whole single set row
-//         /updateSingleSet/1     /2         /3         /5   /11  /11  /3   /7   /9   /2   /8
 app.post("/updateSingleSet/:setID/:hPlayerID/:aPlayerID/:g1h/:g1a/:g2h/:g2a/:g3h/:g3a/:g4h/:g4a", function (req,res) {
 
         var setID =   req.params.setID;
@@ -265,17 +264,23 @@ app.post("/updateSingleSet/:setID/:hPlayerID/:aPlayerID/:g1h/:g1a/:g2h/:g2a/:g3h
         var g4a = req.params.g4;
         console.log("hplayer value ="+hplayer)
         console.log("aplayer value ="+aplayer)
-    connection.query("UPDATE conp2.singleset SET hplayer = '"+hplayer+"', aplayer = '"+aplayer+"', g1h = '"+g1h+"', g1a = '"+g1a+"', g2h = '"+g2h+"',g2a = '"+g2a+"',g3h = '"+g3h+"',g3a = '"+g3a+"',g4h = '"+g4h+"',g4a = '"+g4a+"' WHERE id ="+ setID, function (error, rows, fields){
-        // callback aka when the query is done this fires
-        if (!!error){
-            console.log("Error in the query");
-            console.log(error);
-        } else {
-            console.log(rows);
-            console.log("allMatches sets Successful query");
-            res.send(rows);
+        if (aplayer == null || aplayer == "null"){
+            console.log("hello there is an error aplayer= "+aplayer)
         }
-    });
+
+        connection.query("UPDATE conp2.singleset SET hplayer = '"+hplayer+"', aplayer = '"+aplayer+"', g1h = '"+g1h+"', g1a = '"+g1a+"', g2h = '"+g2h+"',g2a = '"+g2a+"',g3h = '"+g3h+"',g3a = '"+g3a+"',g4h = '"+g4h+"',g4a = '"+g4a+"' WHERE id ="+ setID, function (error, rows, fields){
+            // callback aka when the query is done this fires
+            if (!!error){
+                console.log("Error in the query");
+                console.log(error);
+            } else {
+                console.log(rows);
+                console.log("allMatches sets Successful query");
+                res.send(rows);
+            }
+        });
+
+
 
 });
 
